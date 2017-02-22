d69be5fe64b99e7bb472942dd22b6c0594ebb836
chasen-bettinger <chasen-bettinger@users.noreply.github.com>
2017-02-22 11:04:23 -0500

    Finished Lesson 1

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,38 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+var bio = {
+  name: 'Chasen Bettinger',
+  role: 'Developer',
+  contact: 'bettingerchasen@gmail.com',
+  phone: '678-882-9122',
+  picture: 'images/fry.jpg',
+  welcome_message: 'Hello and welcome to my site!',
+  skills: [
+    "Programming", ' Storytelling', ' Building Businesses'
+  ],
+  education: [{
+		name: "West Forsyth High School",
+		years: "2011-2015",
+		fav_teacher: "Robertson"
+	}, {
+		name: "Appalachian State University",
+		years: "2015-2018",
+		fav_teacher: "Hunsinger"
+	}],
+  projects: {
+    start: "1/2017",
+    title: "Rooibus",
+    dates: "Finish Website by 3/15/2017",
+    description: "Rooibus is a learning environment focused on the mastery of STEAM subjects",
+    picture: "images/robotv2.jpg"
+  }
+  work: {
+    start: "8/2015",
+    employer: "Appalachian State University",
+    title: "IT Consultant",
+    location: "Boone, NC",
+    description: "Fix students hardware and software issues"
+  }
+};
